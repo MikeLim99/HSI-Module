2bd0ef946abef5756830a02cb13f45cd534c577c
unknown <m.lim271999@gmail.com>
2026-01-28 16:07:33 +0800

    test

--- src/pages/CareerPage.jsx
+++ src/pages/CareerPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import PagePlaceholder from './PagePlaceholder'
+import CareerPageHeader from '../components/CareerPageComponents/CareerPageHeader.jsx'
 
 export default function CareerPage() {
-  return <PagePlaceholder title="Career & Job Opportunities" />
+  return (
+    <>
+    <CareerPageHeader />
+    </>
+
+)
 }
