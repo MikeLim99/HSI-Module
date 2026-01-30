320aa4a7382721a0dd396d7187a119d79ce8b641
unknown <m.lim271999@gmail.com>
2026-01-30 10:18:19 +0800

    100% completed

--- src/components/CareerPageComponents/SelectionBox.jsx
+++ src/components/CareerPageComponents/SelectionBox.jsx
@@ -3,11 +3,18 @@ import React from 'react'
 export default function SelectionBox({ test, className }) {
   return (
     <>
-    <select className={`w-[100%] bg-gray-100 p-2 border border-gray-400 rounded-xl h-10 ${className}`}>
-        {(test.map((option, index) => (
-            <option key={index} value={option.value}>{option.label}</option>
-        )))}
-    </select>  
+    <div className='relative w-full'>
+      <select className={`w-[100%] appearance-none  bg-gray-100 pl-3 border border-gray-300 rounded-xl h-10 text-gray-400 ${className}`}>
+          {(test.map((option, index) => (
+              <option key={index} value={option.value}>{option.label}</option>
+          )))}
+      </select>
+      <div className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2">
+        <svg className="w-4 h-4 text-gray-500" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+        </svg>
+      </div>
+    </div>  
     </>
   )
 }
--- src/pages/CareerPage.jsx
+++ src/pages/CareerPage.jsx
@@ -17,14 +17,16 @@ export default function CareerPage() {
           <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
           <input className='bg-gray-100 pl-10 p-2 w-full border border-gray-300 rounded-xl focus:outline focus:outline-yellow-400 h-10' type="text" placeholder="Search" />      
         </div>
-          <SelectionBox className="w-[30%]" test={[{value: "option1", label: "Newest"}, {value: "option2", label: "Oldest"}]} />                  
+        <div className='w-[30%]'>
+          <SelectionBox test={[{value: "option1", label: "Newest"}, {value: "option2", label: "Oldest"}]} />                  
+        </div>
       </div>
       <div className='w-[90%] bg-white rounded-b-xl justify-center items-center flex gap-5 p-5 shadow-md'>
           <SelectionBox test={[{value: "option1", label: "Department"}, {value: "option2", label: "Quality Assurance"}, {value: "option2", label: "Web Development"}, {value: "option2", label: "IT Sales"}]} />         
           <SelectionBox test={[{value: "option1", label: "Status"}, {value: "option2", label: "Urgent"}, {value: "option2", label: "New"}]} />         
           <SelectionBox test={[{value: "option1", label: "Role"}, {value: "option2", label: "Web Developer - Intern"}, {value: "option2", label: "Quality Assurance - Intern"}]} />         
           <SelectionBox test={[{value: "option1", label: "Tag"}, {value: "option2", label: "Tag 1"}, {value: "option2", label: "Tag 2"}]} />                  
-          <button className='bg-yellow-500 p-2 w-[100%] border border-gray-300 rounded-xl mr-5'><span className='text-white'>CLEAR</span></button>
+          <button className='bg-yellow-500 p-2 w-[100%] border border-gray-300 rounded-lg'><span className='text-white'>CLEAR</span></button>
       </div>
 
 {/* -------------------------------------------------------------------------- */}
